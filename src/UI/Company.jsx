f111664f6f26430a1c62a0692fd26c2a2ee22c11
sohagroy7223 <freelancersohagroy@gmail.com>
2026-07-14 14:20:42 +0600

    addad all compamy logo marquee this page

---
+++ src/UI/Company.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+
+const Company = ({ companies }) => {
+  return (
+    <div className="overflow-hidden w-full">
+      <motion.div
+        className="flex items-center gap-10 w-max"
+        animate={{ x: ["0%", "-20%"] }}
+        transition={{
+          duration: 20,
+          ease: "linear",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      >
+        {[...companies, ...companies].map((company, index) => (
+          <img
+            key={index}
+            src={company.company_log}
+            alt="company logo"
+            className="w-20  h-18 rounded-md object-contain"
+          />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Company;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../component/Banner";
 import Popular from "../UI/Popular";
 import useAxios from "../Hook/useAxios";
 import Feature from "../UI/Feature";
+import Company from "../UI/Company";
 
 const Home = () => {
   const [popularJob, setPopularJob] = useState([]);
   const [featureJob, setFeatureJob] = useState([]);
+  const [allCompany, setAllCompany] = useState([]);
   const instance = useAxios();
 
-  useState(() => {
+  useEffect(() => {
     instance.get("/popular-jobs").then((res) => {
       // console.log(res.data);
       setPopularJob(res.data);
@@ -17,6 +19,10 @@ const Home = () => {
     instance.get("/featured-jobs").then((res) => {
       // console.log(res.data);
       setFeatureJob(res.data);
+    });
+    instance.get("/all-company").then((res) => {
+      // console.log(res.data);
+      setAllCompany(res.data);
     });
   }, []);
   // console.log(popularJob);
@@ -33,13 +39,22 @@ const Home = () => {
           <Popular key={popular._id} popular={popular}></Popular>
         ))}
       </div>
+      <h3 className="text-3xl font-bold mb-5 text-center mt-3">
+        All <span className="text-primary">Company</span>
+      </h3>
+      <div className="flex gap-3 items-center">
+        {/* {allCompany.map((company) => (
+          <Company key={company._id} company={company}></Company>
+        ))} */}
+        <Company companies={allCompany} />
+      </div>
       {/* featured job */}
       <h3 className="text-3xl font-bold mb-5 text-center mt-3">
         Featured <span className="text-primary">job</span>
       </h3>
       <div className="grid  md:grid-cols-3 lg:grid-cols-4 gap-3 ">
         {featureJob.map((feature) => (
-          <Feature feature={feature}></Feature>
+          <Feature key={feature._id} feature={feature}></Feature>
         ))}
       </div>
     </div>

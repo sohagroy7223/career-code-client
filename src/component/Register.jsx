75970eb1315c462f8d5fac897a274174501a4f8c
sohagroy7223 <freelancersohagroy@gmail.com>
2026-07-17 14:37:57 +0600

    addad signUp user functionality this page now user can registr this page

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+import useAxios from "../Hook/useAxios";
 
 const Register = () => {
+  const { createUser } = use(AuthContext);
+  const instance = useAxios();
+
+  const handelRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // console.log(name, image, email, password);
+
+    createUser(email, password).then((result) => {
+      // console.log(result);
+
+      instance
+        .post("/users", {
+          displayName: `${result.user.displayName}`,
+          email: `${result.user.email}`,
+          image: `${result.user.photoURL}`,
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    });
+  };
   return (
     <div
       className="h-screen bg-cover mx-auto items-center flex flex-col justify-center bg-center  text-gray-200 rounded-md"
       style={{ backgroundImage: "url('login.jpg')" }}
     >
       <h1 className="text-3xl font-bold text-center mb-6">Register now!</h1>
-      <form className="fieldset max-w-md mx-auto border backdrop-blur-sm rounded-2xl p-10">
+      <form
+        onSubmit={handelRegister}
+        className="fieldset max-w-md mx-auto border backdrop-blur-sm rounded-2xl p-10"
+      >
         <label className="">Name</label>
         <input
           type="text"
           className="input text-black"
           placeholder="Your Name"
           name="name"
+          autoComplete="username"
           required
         />
         <label className="">Photo URL</label>
@@ -31,6 +62,7 @@ const Register = () => {
           className="input text-black"
           placeholder="Email"
           name="email"
+          autoComplete="email"
           required
         />
         <label className="">Password</label>
@@ -39,6 +71,7 @@ const Register = () => {
           className="input text-black"
           placeholder="Password"
           name="password"
+          autoComplete="current-password"
           required
         />
 
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
 import { auth } from "../Firebase/Firebase.init";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
   };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
   const userInfo = {
     signInWithGoogle,
+    createUser,
   };
 
   return <AuthContext value={userInfo}>{children}</AuthContext>;

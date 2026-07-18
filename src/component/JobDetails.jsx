bf493c7bbf01d21b9986112d8eeb7849edfd69a8
sohagroy7223 <freelancersohagroy@gmail.com>
2026-07-18 15:30:58 +0600

    addad job details

--- src/component/JobDetails.jsx
+++ src/component/JobDetails.jsx
@@ -23,6 +23,8 @@ const JobDetails = () => {
     title,
     vacancy,
     workplace,
+    hr_name,
+    hr_email,
   } = data;
   return (
     <div>
@@ -57,7 +59,74 @@ const JobDetails = () => {
           </div>
         </li>
       </div>
-      <div></div>
+      <div className="bg-base-100 mt-10 p-3 flex w-full gap-5 ">
+        <div className="space-y-6 p-5 bg-gray-100 w-[70%] rounded-lg">
+          <div>
+            <h3 className="font-bold text-lg">Job Description</h3>
+            <p>{description}</p>
+          </div>
+          <div>
+            <h3 className="font-bold text-lg">responsibilities</h3>
+            <div>
+              {responsibilities.map((responsibility, index) => (
+                <li key={index} className="doted">
+                  <p>{responsibility}</p>
+                </li>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3 className="font-bold text-lg">requirements</h3>
+            <div>
+              {requirements.map((requirement, index) => (
+                <li key={index} className="doted">
+                  <p>{requirement}</p>
+                </li>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3>
+              Hr Name: <span className="font-bold">{hr_name}</span>
+            </h3>
+            <h3>
+              Hr Name: <span className="font-bold">{hr_email}</span>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <span className="font-bold">Date</span>: {applicationDeadline}
+            </h3>
+          </div>
+        </div>
+        <div className=" p-5 w-[30%] bg-gray-100 rounded-lg space-y-5">
+          <h3 className="font-bold text-center text-lg">Job Details</h3>
+          <div>
+            <p className="text-lg">Job Title : </p>
+            <h3 className="font-bold text-lg">{title}</h3>
+          </div>
+          <div>
+            <p className="text-lg">Company : </p>
+            <h3 className="font-bold text-lg">{company}</h3>
+          </div>
+          <div>
+            <p className="text-lg">Job Type : </p>
+            <h3 className="font-bold text-lg">{jobType}</h3>
+          </div>
+          <div>
+            <p className="text-lg"> Work Place : </p>
+            <h3 className="font-bold text-lg">{workplace}</h3>
+          </div>
+          <div>
+            <p className=" text-lg"> Job Location : </p>
+            <h3 className="font-bold text-lg">{location}</h3>
+          </div>
+          <div>
+            <p className=" text-lg">Salary : </p>
+            <h3 className="font-bold text-lg">{salary}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

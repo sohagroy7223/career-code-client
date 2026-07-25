907259d241399f845f21ba54e6318b0bb20b812a
sohagroy7223 <freelancersohagroy@gmail.com>
2026-07-25 18:35:18 +0600

    addad y application some details and add toggel btn for see all details

--- src/component/MyAllApplication.jsx
+++ src/component/MyAllApplication.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { IoLocation } from "react-icons/io5";
+import React, { useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { IoDocument, IoLocation } from "react-icons/io5";
 import { MdHourglassFull } from "react-icons/md";
 import { VscRemoteExplorer } from "react-icons/vsc";
 
 const MyAllApplication = ({ apply }) => {
-  const { jobId, companyLogo, title, company, jobType, workplace, location } =
-    apply;
+  const [showDetails, setShowDetails] = useState(false);
+  const {
+    jobId,
+    companyLogo,
+    title,
+    company,
+    jobType,
+    workplace,
+    location,
+    name,
+    email,
+    phone,
+    experience,
+    skill,
+    portfolio,
+    github,
+    linkedin,
+    coverLater,
+  } = apply;
+
+  const handelBtnToggle = () => {
+    setShowDetails(!showDetails);
+  };
 
   return (
-    <div className="list rounded-box shadow-md">
+    <div className="list rounded-box shadow-md border border-primary border-dashed">
       <li className="list-row">
         <div>
           <img className="size-15 rounded-box" src={companyLogo} />
@@ -33,9 +55,71 @@ const MyAllApplication = ({ apply }) => {
               </p>
             </div>
           </div>
+          {showDetails && (
+            <div className="mt-5 pt-4 flex justify-center gap-5 bg-gray-100 rounded-2xl">
+              <div className="space-y-3 p-3 border-r border-primary border-dashed">
+                <h3 className="flex items-center gap-3 text-2xl font-medium">
+                  <FaUserCircle size={25} />
+                  Personal Information
+                </h3>
+                <p className="flex items-center gap-2">
+                  <strong>Applicant Name</strong>: {name}
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>Email:</strong> {email}
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>Phone:</strong> {phone}
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>Experience:</strong> {experience}
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>Skills:</strong> {skill}
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>Portfolio:</strong>{" "}
+                  <span className="text-primary cursor-pointer">
+                    <a target="_blank" href={portfolio}>
+                      portfolio
+                    </a>
+                  </span>
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>GitHub :</strong>{" "}
+                  <span className="text-primary cursor-pointer">
+                    <a target="_blank" href={github}>
+                      GitHub
+                    </a>
+                  </span>
+                </p>
+                <p className="flex items-center gap-2">
+                  <strong>LinkedIn:</strong>{" "}
+                  <span className="text-primary cursor-pointer">
+                    <a target="_blank" href={linkedin}>
+                      Linkedin
+                    </a>
+                  </span>
+                </p>
+              </div>
+              <div className="space-y-3 p-3">
+                <h3 className="flex  gap-3 text-2xl font-medium">
+                  <IoDocument size={25} />
+                  Cover Letter
+                </h3>
+                <p>{coverLater}</p>
+              </div>
+            </div>
+          )}
         </div>
+
+        <button
+          onClick={handelBtnToggle}
+          className="btn cursor-pointer bg-primary text-gray-100"
+        >
+          {showDetails ? "Hide Details" : "view details"}
+        </button>
       </li>
-      <button className="btn cursor-pointer ">view details</button>
     </div>
   );
 };
